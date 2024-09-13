523654ab2a69a40888589a8a3ea66fe7efe05320
cichengzi <410708554@qq.com>
2024-09-13 11:15:46 +0800

    2024/9/13 11:15

--- content/project/1D-ADAS-Verification/convert.py
+++ content/project/1D-ADAS-Verification/convert.py
@@ -1,12 +1,12 @@
 from pdf2image import convert_from_path
 
 # Path to the PDF file
-pdf_path = 'featured.pdf'
+pdf_path = 'images/reachable_sets.pdf'
 
 # Convert PDF to a list of PIL images
 images = convert_from_path(pdf_path)
 
 # Save each page as a PNG file
 for i, image in enumerate(images):
-    image.save(f'featured.png', 'PNG')
+    image.save(f'reachable_sets.png', 'PNG')
 
